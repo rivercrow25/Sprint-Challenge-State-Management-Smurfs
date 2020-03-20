8fde184e6730bd8c01c28ac0628336e6c0480b26
nick migel <nickmigel2@gmail.com>
2020-03-20 11:38:21 -0400

    set up redux and reducer

---
+++ smurfs/src/reducer/mainReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+    smurf: {
+        name: '',
+        age: '',
+        height: '',
+        id: null
+    },
+    isFetching: false
+}
+
+export const smurfReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
